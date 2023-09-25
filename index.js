499e80f5b2e01b486e8d41fe45ff6497c6d0adf1
chaeaw <chaeaw04@gmail.com>
2023-09-25 14:22:42 +0900

    [update] header item Click 이벤트 추가

--- index.js
+++ index.js
@@ -13,6 +13,9 @@
   const $menuList = get(".menu__list");
   const $menuItems = getAll(".menu__list-item");
 
+  const $headerList = get(".header__list");
+  const $headerItems = getAll(".header__list-item");
+
   window.addEventListener("DOMContentLoaded", () => {
     getClock();
     setInterval(getClock, 3000);
@@ -36,6 +39,18 @@
         }
 
         target.classList.add("active");
+      });
+    });
+
+    $headerItems.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        const $activeItem = $headerList.querySelector(
+          ".header__list-item--inverted"
+        );
+        if ($activeItem)
+          $activeItem.classList.remove("header__list-item--inverted");
+
+        e.target.classList.add("header__list-item--inverted");
       });
     });
   });
